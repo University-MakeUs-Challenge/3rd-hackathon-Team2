c096c653dc1378cc29364ca9456125ea1a7956f9
dldmldlsy <dldmldlsy@naver.com>
2022-11-12 21:22:22 +0900

    프로젝트 생성

---
+++ server/src/test/java/com/umc/hackathon/team2/Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.hackathon.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
