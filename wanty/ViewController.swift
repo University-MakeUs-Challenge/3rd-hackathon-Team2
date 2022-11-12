65599e4625c670b3dd5383ce9b27b0dee81faaa8
binisnull <dbsqlssss0704@gmail.com>
2022-11-12 21:55:48 +0900

    Initial Commit

---
+++ wanty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanty
+//
+//  Created by 박윤빈 on 2022/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
